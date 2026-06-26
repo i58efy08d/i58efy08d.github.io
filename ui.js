1e8d9f679b6dca83178cf5806d63596d2fb10b0e
i58efy08d <nazarenkonazar709@gmail.com>
2026-06-26 11:14:20 +0300

    Delete ui.js

--- ui.js
+++
@@ -1,8 +0,0 @@
-export function renderAdvice(advice, container) {
-    container.innerHTML = `
-        <div class="prophecy-card">
-            <div class="prophecy-id">№${advice.id}</div>
-            <p>${advice.advice}</p>
-        </div>
-    `;
-}
