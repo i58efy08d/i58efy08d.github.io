222fdbfc803fb4514bf8cb14fb0a91080be4b291
i58efy08d <nazarenkonazar709@gmail.com>
2026-06-26 11:13:36 +0300

    Update handlers.js

--- js/handlers/handlers.js
+++ js/handlers/handlers.js
@@ -1,1 +1,11 @@
+import { getProphecy } from '../api/api.js';
+import { renderAdvice } from '../ui/ui.js';
+import { saveAdvice } from '../storage/storage.js';
 
+export async function handleSubmit(container) {
+    const advice = await getProphecy();
+
+    renderAdvice(advice, container);
+
+    saveAdvice(advice);
+}
