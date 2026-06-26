7313b1887f18bae65d9abf96067b71b998ab8f7f
i58efy08d <nazarenkonazar709@gmail.com>
2026-06-26 11:01:34 +0300

    Delete storage.js

--- storage.js
+++
@@ -1,7 +0,0 @@
-export function saveAdvice(advice) {
-    localStorage.setItem('lastAdvice', JSON.stringify(advice));
-}
-
-export function getSavedAdvice() {
-    return JSON.parse(localStorage.getItem('lastAdvice'));
-}
