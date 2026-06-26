400b15884731b23946a4eb56ab016f1384f66ccb
i58efy08d <nazarenkonazar709@gmail.com>
2026-06-26 11:11:30 +0300

    Update api.js

--- js/api/api.js
+++ js/api/api.js
@@ -1,1 +1,10 @@
+export async function getProphecy() {
+    const response = await fetch('https://api.adviceslip.com/advice');
 
+    if (!response.ok) {
+        throw new Error('Не вдалося завантажити дані');
+    }
+
+    const data = await response.json();
+    return data.slip;
+}
