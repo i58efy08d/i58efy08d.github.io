ead2280c9f16172ec5fe6207be5b18dcb90c73c3
i58efy08d <nazarenkonazar709@gmail.com>
2026-06-26 11:12:40 +0300

    Update ui.js

--- js/ui/ui.js
+++ js/ui/ui.js
@@ -1,1 +1,8 @@
-
+export function renderAdvice(advice, container) {
+    container.innerHTML = `
+        <div class="prophecy-card">
+            <div class="prophecy-id">№${advice.id}</div>
+            <p>${advice.advice}</p>
+        </div>
+    `;
+}
